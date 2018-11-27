2d1d868a9dabd70e122dc78dc523cfd9b27468ac
cai471600373 <38424427+cai471600373@users.noreply.github.com>
2018-11-27 12:56:31 +0800

    Create my.js

---
+++ my.js
@@ -0,0 +1,7 @@
+function shipinjiexi(){
+         var a=""
+         var b="http://jx.hanximeng.com/api.php?url="
+         a=document.getElementById("shipinjiexi").value
+         document.getElementById("kj").src=b+a
+
+                      }
