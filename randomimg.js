ed679ed3e1a9993e965120532f066719a9fb23a5
unknown <2532413796@qq.com>
2025-06-16 15:28:42 +0800

    Site updated: 2025-06-16 15:28:30

--- randomimg.js
+++ randomimg.js
@@ -47,6 +47,15 @@ randomimg = function (zhengjing,debug=false){
         }
     this.get_img = function(){
         if(this.pd){
+            /*
+            http://www.dmoe.cc/random.php
+            https://img.xjh.me/random_img.php?type=bg&ctype=acg
+            https://api.vvhan.com/api/wallpaper/acg
+            https://www.loliapi.com/acg/pc/
+            https://api.ixiaowai.cn/gqapi/index.php
+            https://api.r10086.com/%E6%A8%B1%E9%81%93%E9%9A%8F%E6%9C%BA%E5%9B%BE%E7%89%87api%E6%8E%A5%E5%8F%A3.php?%E5%9B%BE%E7%89%87%E7%B3%BB%E5%88%97=%E5%8A%A8%E6%BC%AB%E7%BB%BC%E5%90%881
+
+            */
             return "https://api.vvhan.com/api/wallpaper/acg";//+rand(pc.length-1);
         }else{
             return this.normal()
