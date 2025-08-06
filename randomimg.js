d48aa11de43d70dfdbf3546f28711f8755d45dd4
unknown <2532413796@qq.com>
2025-08-06 23:34:22 +0800

    Site updated: 2025-08-06 23:34:09

--- randomimg.js
+++ randomimg.js
@@ -57,7 +57,7 @@ randomimg = function (zhengjing,debug=false){
             https://api.r10086.com/%E6%A8%B1%E9%81%93%E9%9A%8F%E6%9C%BA%E5%9B%BE%E7%89%87api%E6%8E%A5%E5%8F%A3.php?%E5%9B%BE%E7%89%87%E7%B3%BB%E5%88%97=%E5%8A%A8%E6%BC%AB%E7%BB%BC%E5%90%881
 
             */
-            return "https://img.xjh.me/random_img.php?type=bg&ctype=acg";//+rand(pc.length-1);
+            return "https://img.xjh.me/random_img.php?type=bg&ctype=acg&return=302";//+rand(pc.length-1);
         }else{
             return this.normal()
         }
